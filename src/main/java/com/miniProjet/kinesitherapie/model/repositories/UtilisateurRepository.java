ead5c090783451b0fa1fad9545b3132e84a88c0d
Skeiba <salah.bahlaouane@gmail.com>
2025-01-17 22:26:11 +0100

    removed findAll

--- src/main/java/com/miniProjet/kinesitherapie/model/repositories/UtilisateurRepository.java
+++ src/main/java/com/miniProjet/kinesitherapie/model/repositories/UtilisateurRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
     Utilisateur findByLogin(String login);
-    List<Utilisateur> findAll();
 }

17c83aad83b5e1e52381a84dbc848bbbd93462fd
Skeiba <salah.bahlaouane@gmail.com>
2025-01-28 22:59:39 +0100

    minor changes

--- src/main/java/com/miniProjet/kinesitherapie/model/dto/PatientDTO.java
+++ src/main/java/com/miniProjet/kinesitherapie/model/dto/PatientDTO.java
@@ -1,6 +1,7 @@
 package com.miniProjet.kinesitherapie.model.dto;
 
 import com.miniProjet.kinesitherapie.utils.AppConstants;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -11,13 +12,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class PatientDTO {
-    @NotNull(message = "Nom is required")
+    @NotNull(message = AppConstants.INVALID_NOM)
     private String nom;
 
-    @NotNull(message = "Prenom is required")
+    @NotNull(message = AppConstants.INVALID_PRENOM)
     private String prenom;
 
-    @NotNull(message = "Adresse is required")
+    @NotNull(message = "Email cannot be null")
+    @Email(message = AppConstants.INVALID_EMAIL)
+    private String email;
+
+    @NotNull(message = AppConstants.INVALID_ADRESSE)
     private String adresse;
 
     @Pattern(regexp = "^(06|07|05)[0-9]{8}$", message = AppConstants.INVALID_PHONE_NUMBER)
